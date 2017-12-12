7b754ed0e5d7983ce70b3c898a1ada36288214fe
Mark Rosenberg <mrk.rosenberg@gmail.com>
2017-12-12 14:31:47 -0700

    adds image folder, background image and object constructor for player

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,19 @@
 console.log("connected");
+
+$(document).ready( function (){
+	
+	function Player(id, topCoordinate, bottomCoordinate) {					//creates player objects
+		this.id = id;
+		this.top = topCoordinate;
+		this.bottom = bottomCoordinate;
+	}
+
+	var Rick = new Player("Rick", 0, 0);
+	console.log(Rick);
+
+
+
+
+
+	
+});
