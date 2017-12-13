a74b9cd664e051c3474a0633da2ee0d390e6c90f
Mark Rosenberg <mrk.rosenberg@gmail.com>
2017-12-12 18:32:21 -0700

    adds player constructor, wormhole and wormhole movement

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,29 @@
 console.log("connected");
 
 $(document).ready( function (){
-	
-	function Player(id, topCoordinate, bottomCoordinate) {					//creates player objects
+
+//player constructor function
+	function Player(id, bottomCoordinate, leftCoordinate) {					
 		this.id = id;
-		this.top = topCoordinate;
 		this.bottom = bottomCoordinate;
+		this.left = leftCoordinate; 
 	}
 
-	var Rick = new Player("Rick", 0, 0);
+
+	var Rick = new Player('Rick', 10, 10);
 	console.log(Rick);
 
+	$('#rickStart').append('<div class="Rick" style="bottom:' + Rick.bottom + '; left:' + Rick.left + ';"></div>');
+
+//stores all gameBoard spaces in "location"
+	var location = $('.col-sm-1').toArray();										
+	console.log(location);
+
+
+//creates portal element & attaches it to portalStart location
+	var portal = $('<div id="wormHitZone"><img id="wormHole" src="../images/portal.jpg" alt="portal" /></div>').appendTo($('#portalStart'));		 
+	setInterval( function () {portal.appendTo(location[Math.floor((Math.random() * 100) + 1)]);}, 1000);
+	console.log(portal);
 
 
 
